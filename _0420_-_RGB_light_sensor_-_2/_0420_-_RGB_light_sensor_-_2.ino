f53580c8471d54e9e368c501533b0a9c760c124f
Sanjay2521 <137406694+Sanjay2521@users.noreply.github.com>
2023-07-04 19:02:54 +0530

    Update _0420_-_RGB_light_sensor_-_2.ino

--- _0420_-_RGB_light_sensor_-_2/_0420_-_RGB_light_sensor_-_2.ino
+++ _0420_-_RGB_light_sensor_-_2/_0420_-_RGB_light_sensor_-_2.ino
@@ -1,42 +1,4 @@
-/*  RGB light sensor demo sketch
- * 
- * This sketch gets a color RGB reading from the light sensor 
- * on the Adafruit TCS34725 (or equivelant) breakout module.
- * 
- * This sketch was adapted from the original that comes with the 
- * Adafruit library for Arduino Step by Step by Peter Dalmaris.
- * 
- * Components
- * ----------
- *  - Arduino Uno
- *  - Adafruit RGB TCS34725 sensor breakout or equivelant
- *  
- *  Libraries
- *  ---------
- *  - Wire
- *  - Adafruit_TCS34725
- *
- * Connections
- * -----------
- *  Break out    |    Arduino Uno
- *  -----------------------------
- *      VIN      |      5V
- *      GND      |      GND
- *      SCL      |      SCL or A5
- *      SDA      |      SDA or A4
- *      LED      |      GND to turn off (or any digital out set to LOW)
- *      INT      |      2 (but not used in this sketch)
- * 
- * Other information
- * -----------------
- *  For information on RGB color: https://en.wikipedia.org/wiki/RGB_color_model
- *  For information on color temperature: https://en.wikipedia.org/wiki/Color_temperature
- *  For more information on the breakout see https://www.adafruit.com/product/1334
- *  The repository for the library used is at https://github.com/adafruit/Adafruit_TCS34725
- *  
- *  Created on October 8 2016 by Peter Dalmaris
- * 
- */
+
  
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
