43bcae86bc9c5443e379c152176fbd4720d40e0c
Sanjay2521 <137406694+Sanjay2521@users.noreply.github.com>
2023-07-04 19:28:42 +0530

    Update Smart_bridge_1.ino

--- Smart_bridge_1.ino
+++ Smart_bridge_1.ino
@@ -1,3 +1,14 @@
+/*
+  this  code is designed keeping in mind that, to avoid the overflow of water a
+over the bridge,
+  the advantage is :
+1) increase the road life
+in this project  components that I am using  are:
+2 servo motors 
+1 moisture sensor 
+Arduino  UNO
+*\
+
 #include <Servo.h>
 
 #include <Servo.h>
@@ -5,8 +16,8 @@
 
 Servo tap_servo;
 
-int sensor_pin = 5;
-int tap_servo_pin =4;
+int sensor_pin = 5;   //moisture sensor read pin
+int tap_servo_pin =4; // servo motor defined 
 int val;
 
 void setup(){
