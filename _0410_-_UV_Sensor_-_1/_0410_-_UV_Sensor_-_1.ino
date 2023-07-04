bcf8bf2eff569cdb853e29c0cce23d7856dc056c
Sanjay2521 <137406694+Sanjay2521@users.noreply.github.com>
2023-07-04 18:59:37 +0530

    Update _0410_-_UV_Sensor_-_1.ino

--- _0410_-_UV_Sensor_-_1/_0410_-_UV_Sensor_-_1.ino
+++ _0410_-_UV_Sensor_-_1/_0410_-_UV_Sensor_-_1.ino
@@ -1,41 +1,4 @@
-/*  UV sensor demonstration sketch
- * 
- * This sketch reads the state of the Adafruit analog UV sensor.
- * It then converst the analog reading to a voltage, and then
- * the voltage to an actual UV index value.
- * 
- * This sketch was written for Arduino Step by Step by Peter Dalmaris.
- * 
- * Components
- * ----------
- *  - Arduino Uno
- *  - Adafruit UV sensor breakout or equivelant
- *  
- *  Libraries
- *  ---------
- *  NONE
- *
- * Connections
- * -----------
- *  Break out    |    Arduino Uno
- *  -----------------------------
- *      +        |         5V
- *      -        |         GND
- *      Out      |         A0
- *      
- * 
- * Other information
- * -----------------
- * This sensor can detect UV light at 240-370nm.
- * This contains UVA and most of UVB.
- * For more information on UV light see https://en.wikipedia.org/wiki/Ultraviolet
- * For more information on the breakout see https://www.adafruit.com/product/1918
- * For information on the UV index scale see https://www.epa.gov/sunsafety/uv-index-scale-1
- * Sensor datasheet: https://cdn-shop.adafruit.com/datasheets/1918guva.pdf
- *      
- *  Created on October 8 2016 by Peter Dalmaris
- * 
- */
+
 
 // the setup routine runs once when you press reset:
 void setup() {
